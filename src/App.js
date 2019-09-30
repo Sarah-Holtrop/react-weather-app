4cc0b8888985c935b4456a6be388e439771d3cd9
Sarah-Opalenik <sarahmo93@gmail.com>
2019-09-30 12:31:05 -0600

    weather api and cors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  let h1 = document.querySelector('h1')
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude
+
+      const proxy = 'https://cors-anywhere.herokuapp.com/'
+      const api = `${proxy}https://api.darksky.net/forecast/7feeba086d5bba97a3311db9757129cb/${lat},${long}`
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data)
+          const { temperature, summary } = data.currently;
+
+        })
+    });
+
+  } else {
+    h1.innerHTML = "Please enable geolocation"
+  }
+})
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
     </div>
   );
 }
